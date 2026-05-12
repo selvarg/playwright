b3b606934e02d3b205a72decf7c69f92226b5f01
selvarg <106023183+selvarg@users.noreply.github.com>
2026-05-12 15:35:41 +0530

    Remove commented-out add to cart tests
    Removed commented-out test cases for adding items to the cart.

--- tests/task.spec.js
+++ tests/task.spec.js
@@ -41,47 +41,5 @@ console.log('Minimum price:', minPrice);
   }
 
 });
-// test('add to cart',async({page})=>{
-//   await page.goto("https://www.amazon.in/");
-//   await page.locator("input#twotabsearchtextbox").fill("camera");
-//   await page.locator("input#nav-search-submit-button").click();
-//   //await page.locator("//span[contains(text(),'vivo V70 FE 5G')]/ancestor::div[@class='puisg-col-inner']//button[@aria-label='Add to cart']").click()
-//   await page.locator('//span[text()="CP PLUS 4MP Quad HD Smart Wi-Fi CCTV Camera for Home | 360° Pan & Tilt | CTC Cyber Secure Tech | Supports LAN | View & Talk | Smart Detection Suite | Night Vision | Cloud Storage | OK Google | CP-E41Q"]').click()
-//   await page.waitForSelector("//span[@id='submit.add-to-cart-announce']");
-//   await page.locator("//span[@id='submit.add-to-cart-announce']").click();
-//   await page.locator("span#attachSiNoCoverage-announce").click();
-//   const res = await page.locator("div#NATC_MWEB_FULL_PAGE_CONF_MSG_SUCCESS")
-//   console.log(res.textContent());
-//   await page.waitForTimeout(5000);
-
-// })
-
-// test('add to cart', async ({ page }) => {
-
-//   await page.goto("https://www.amazon.in/");
-
-//   await page.fill("#twotabsearchtextbox", "camera");
-//   await page.click("#nav-search-submit-button");
-
-//   // click product + capture new tab
-//   const [newPage] = await Promise.all([
-//     page.context().waitForEvent('page'),
-//     page.locator('//span[contains(text(),"CP PLUS")]').first().click()
-//   ]);
-//   // wait for new tab to load
-//   await newPage.waitForLoadState();
-// await newPage.pause();
-//   // click add to cart
-//    const popup=await newPage.locator('//input[@id="add-to-cart-button"]')
-// await popup.waitFor();
-// await popup.click()
-
-//    const  frame= await newPage.locator('//input[@aria-labelledby="attachSiNoCoverage-announce"]')
-// await frame.waitFor({ state: 'visible' });
-// await frame.click();
-//   // get confirmation text
-//   const res = await newPage.locator('//h4[normalize-space(text())="Added to cart"]').innerText();
-
-//   console.log("confirm:",res);
 
 // });
